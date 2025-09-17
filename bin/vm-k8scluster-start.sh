01ad616d64f72d722eaf762f2d3eac4a977c2904
Steven Min <steven.jsmin@gmail.com>
2025-09-17 10:52:29 +1000

    add showing instance name and public ip

--- bin/vm-k8scluster-start.sh
+++ bin/vm-k8scluster-start.sh
@@ -5,3 +5,10 @@ for OUTPUT in $(aws ec2 describe-instances --filters "Name=instance-state-name,V
 do
     aws ec2 start-instances --instance-ids ${OUTPUT}   
 done
+
+sleep 10
+
+aws ec2 describe-instances \
+  --filters "Name=instance-state-name,Values=running,pending" \
+  --query 'Reservations[].Instances[?contains(Tags[?Key==`Name`].Value | [0], `k8s`)].[Tags[?Key==`Name`].Value | [0], PublicIpAddress]' \
+  --output table  
