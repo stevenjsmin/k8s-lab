e578af7256c21ac73f464120f6672c88a8b42194
Steven Min <steven.jsmin@gmail.com>
2025-09-22 11:11:31 +1000

    Enhance cli control scripts

--- bin/vm-k8s-start.sh
+++ bin/vm-k8s-start.sh
@@ -1,9 +1,8 @@
-#!/bin/sh
-
+#!/usr/bin/env bash
 
 for OUTPUT in $(aws ec2 describe-instances --filters "Name=instance-state-name,Values=stopped" --output text --query 'Reservations[].Instances[?contains(Tags[?Key==`Name`].Value | [0], `k8s`)].InstanceId')
 do
-    aws ec2 start-instances --instance-ids ${OUTPUT}   
+    aws ec2 start-instances --instance-ids ${OUTPUT}
 done
 
 sleep 10
@@ -11,4 +10,10 @@ sleep 10
 aws ec2 describe-instances \
   --filters "Name=instance-state-name,Values=running,pending" \
   --query 'Reservations[].Instances[?contains(Tags[?Key==`Name`].Value | [0], `k8s`)].[Tags[?Key==`Name`].Value | [0], PublicIpAddress]' \
-  --output table  
+  --output table
+
+
+# AWS CLI에서 PublicIP와 Name을 탭으로 구분해 출력
+instances=$(aws ec2 describe-instances \
+  --query 'Reservations[].Instances[].[PublicIpAddress, Tags[?Key==`Name`].Value|[0]]' \
+  --output text)
